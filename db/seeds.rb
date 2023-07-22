db06d2b3fc710e31de151aa19df3c2dcec0b3cca
Weston Schutt <wtschutt@gmail.com>
2023-07-22 12:44:39 -0600

    Test: Add tests for US 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+EmployeeTicket.destroy_all
+Employee.destroy_all
+Ticket.destroy_all
+Department.destroy_all
+
+  @cx = Department.create!(name: 'CX', floor: "Third" )
+  @michael = @cx.employees.create!(name: 'Michael', level: 2)
+  @emily = @cx.employees.create!(name: 'Emily', level: 5)
+
+  @ticket1 = Ticket.create!(subject: 'Update profile bug', age: 28)
+  @ticket2 = Ticket.create!(subject: 'Infinitie Loop', age: 21)
+  @ticket3 = Ticket.create!(subject: 'Page not loading', age: 14)
+  @ticket4 = Ticket.create!(subject: 'view on mobile broken', age: 19)
+
+  @employee_ticket1 = EmployeeTicket.create!(employee_id: @michael.id, ticket_id: @ticket1.id)
+  @employee_ticket2 = EmployeeTicket.create!(employee_id: @michael.id, ticket_id: @ticket2.id)
+  @employee_ticket3 = EmployeeTicket.create!(employee_id: @michael.id, ticket_id: @ticket3.id)
+  @employee_ticket4 = EmployeeTicket.create!(employee_id: @emily.id, ticket_id: @ticket3.id) # shares ticket with michael
+  @employee_ticket5 = EmployeeTicket.create!(employee_id: @emily.id, ticket_id: @ticket4.id)
+    
--- spec/features/employees/show_spec.rb
+++ spec/features/employees/show_spec.rb
@@ -10,10 +10,13 @@ RSpec.describe 'Employee #show page', type: :feature do
       @ticket1 = Ticket.create!(subject: 'Update profile bug', age: 28)
       @ticket2 = Ticket.create!(subject: 'Infinitie Loop', age: 21)
       @ticket3 = Ticket.create!(subject: 'Page not loading', age: 14)
+      @ticket4 = Ticket.create!(subject: 'view on mobile broken', age: 19)
 
       @employee_ticket1 = EmployeeTicket.create!(employee_id: @michael.id, ticket_id: @ticket1.id)
       @employee_ticket2 = EmployeeTicket.create!(employee_id: @michael.id, ticket_id: @ticket2.id)
       @employee_ticket3 = EmployeeTicket.create!(employee_id: @michael.id, ticket_id: @ticket3.id)
+      @employee_ticket4 = EmployeeTicket.create!(employee_id: @emily.id, ticket_id: @ticket3.id) # shares ticket with michael
+      @employee_ticket5 = EmployeeTicket.create!(employee_id: @emily.id, ticket_id: @ticket4.id)
     
       visit "/employees/#{@michael.id}"
     end
@@ -37,6 +40,26 @@ RSpec.describe 'Employee #show page', type: :feature do
         expect(page).to have_content("Subject: #{@ticket1.subject}")
         expect(page).to have_content("Age of Ticket: #{@ticket1.age}")
       end
+    end
+
+    it 'does not show tickets that are not assigned to the employee' do
+      expect(page).to_not have_content(@ticket4.subject) # Only Emily's ticket
+    end
+
+    it 'has a form to add an existing ticket to an employee' do
+      expect(page).to have_css("#add_ticket")
+      expect(page).to have_button("Submit")
+    end
+
+    it 'redirects to the show page and displays added ticket' do
+      within("#add_ticket") do
+        fill_in :ticket_id, with: @ticket4.id
+        click_button "Submit"
+      end
+      
+      save_and_open_page
+      expect(current_path).to eq("/employees/#{@michael.id}")
+      expect(page).to have_content(@ticket4.subject)
     end
   end
 end
